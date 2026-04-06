e987ca3fd4d0d43313f9966c7e29e9fcbe3a36b4
unknown <jjerez@uade.edu.ar>
2026-04-06 02:32:02 -0300

    feat(producto): refactor relacion Producto-Categoria de ManyToOne a ManyToMany

--- e-commerce3/src/main/java/com/uade/tpo/e_commerce3/repository/ProductoRepository.java
+++ e-commerce3/src/main/java/com/uade/tpo/e_commerce3/repository/ProductoRepository.java
@@ -3,13 +3,18 @@ package com.uade.tpo.e_commerce3.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.uade.tpo.e_commerce3.model.Categoria;
 import com.uade.tpo.e_commerce3.model.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    List<Producto> findByCategoria(Categoria categoria);
+
+    // Busca productos que tengan una categoria con ese id
+    @Query("SELECT p FROM Producto p JOIN p.categorias c WHERE c.id = :categoriaId")
+    List<Producto> findByCategoriaId(@Param("categoriaId") Long categoriaId);
+
     List<Producto> findAllByOrderByNombreAsc();
 }
